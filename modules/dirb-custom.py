665729c75f9f9d1dbeb7293e329df9ffd9dbd6a8
Ciaran McNally <mak@redbrick.dcu.ie>
2019-02-05 19:58:36 +0000

    some fixes..

--- modules/dirb-custom.py
+++ modules/dirb-custom.py
@@ -4,18 +4,23 @@ from lib.requestObject import RequestObject
 from lib.fileOp import FileOp
 from lib.dataparser import ParseArguments
 from lib.urlObject import UrlObject
+import sys
 
 class DirbCustom(IPlugin):
     def gen(self, cwd, urls, proxy, headers, timeout, cookies, postdata, 
             module, datalist):
         requestList = []   #Store generated request objects
-        data = FileOp(datalist[0]).reader()
+        try:
+            data = FileOp(datalist[0]).reader()
+        except:
+            print('dirb-custom: Provide a wordlist using -dl [wordlist]')
+            sys.exit(0)
         for url in urls:
             u = UrlObject(url)
-                for directory in data:
-                    newurl = u.u_d + directory
-                    req_get = RequestObject('reqID',"GET", proxy, headers, 
-                                            timeout, cookies, newurl, postdata,
-                                            module)
-                    requestList.append(req_get)
+            for directory in data:
+                newurl = u.u_d + directory
+                req_get = RequestObject('reqID',"GET", proxy, headers, 
+                                        timeout, cookies, newurl, postdata,
+                                        module)
+                requestList.append(req_get)
         return requestList
